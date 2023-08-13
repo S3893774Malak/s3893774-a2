bc96d8b80b56a6646bc823db40332abcd5ac4e3a
S3893774Malak <107348391+S3893774Malak@users.noreply.github.com>
2023-08-13 15:02:49 +0500

    transaction added

---
+++ WebApplication3/Models/Transfer.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Transfer
+    {
+        public int PersonalAccountId { get; set; }
+        public int DestinationAccountId { get; set; }
+        public int Amount { get; set; }
+        public string? Comment { get; set; }
+    }
+}
